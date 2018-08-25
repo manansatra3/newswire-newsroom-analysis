6f39dcf3b548ba5f41c280064c782a32289d935e
Manan Satra <msatra@stevens.edu>
2018-08-25 19:50:07 -0400

    category dictionary

--- newsroomAnalysis.py
+++ newsroomAnalysis.py
@@ -1,39 +1,39 @@
 
 # coding: utf-8
 
-# In[85]:
+# In[1]:
 
 import requests
 from bs4 import BeautifulSoup
 
 
-# In[158]:
+# In[2]:
 
 locationDictionary={}
 categoryDictionary={}
 tagsDictionary={}
 
 
-# In[86]:
+# In[3]:
 
-dateInput=raw_input("Enter a VALID date in MM-DD-YYYY format. Example: 01-31-2018\t")
+dateInput=raw_input("Enter a VALID date in MM-DD-YYYY format. Example: 01-31-2018\n")
 print(dateInput)
 month=int(dateInput.split("-")[0])
 date=int(dateInput.split("-")[1])
 year=int(dateInput.split("-")[2])
-print(date)
-print(month)
-print(year)
+# print(date)
+# print(month)
+# print(year)
 
 
-# In[87]:
+# In[4]:
 
 page = requests.get("https://www.newswire.com/newsroom")    
 if page.status_code==200:   
     soup = BeautifulSoup(page.content, 'html.parser') 
 
 
-# In[159]:
+# In[7]:
 
 def monthToIntMonth(articleMonth):
     switcher = {
@@ -61,32 +61,43 @@ def getSoupArticle(url):
     return soupArticle
 
 def getArticleData(soupArticle):
-    category=""
-    tags=""
-    locList=soupArticle.select("p strong.date-line.color-pr")[0].get_text().strip().split(",")[:-2]
-    location=""
+    
+    locList=soupArticle.select("p strong.date-line.color-pr")[0].get_text().strip().lower().split(",")[:-2]
+    location=[]
     for loc in locList:
-        location=(location+str(loc)).lower();
+        loc=loc.encode('ascii').lstrip().rstrip()
+        location.append(loc)
     print location
-    if location in locationDictionary.keys():
-        locationDictionary[location]=+1
-    else:
-        locationDictionary[location]=1
+    for loc in location:     
+        if loc in locationDictionary.keys():
+            locationDictionary[loc]+=1
+        else:
+            locationDictionary[loc]=1
+        
+    catSoup=soupArticle.select("p[class=mb-0]")[0]
+    catList=str(catSoup.select("a")[0].get_text().encode('ascii').lower()).split(",")
+    for cat in catList:
+        cat=cat.lstrip()
+        if cat in categoryDictionary.keys():
+            categoryDictionary[cat]+=1
+        else:
+            categoryDictionary[cat]=1
 
 
-# In[160]:
+# In[8]:
 
 divs=soup.select("div.news-item-body")
-# print divs
+
 for div in divs:
     p=div.select("time.ln-date")[0]
     articleDate=p.get_text().split(" ")
     articleDay=int(str(articleDate[1]).replace(',',''))
-    print "Day:", articleDay
+#     print "Day:", articleDay
     articleMonth=int(monthToIntMonth(str(articleDate[0]).strip()))
-    print "Month:", articleMonth
+#     print "Month:", articleMonth
     articleYear=int(articleDate[2])
-    print "Year:", articleYear
+#     print "Year:", articleYear
+    print str(articleMonth) + "/" + str(articleDay) + "/" + str(articleYear)
     if(articleYear>=year):
         if(articleMonth>=month):
             if(articleDay>=date):
@@ -94,7 +105,6 @@ for div in divs:
                 print url
                 soupArticle=getSoupArticle(url)
                 getArticleData(soupArticle)
-                continue
             else:
                 break
         else:
@@ -103,6 +113,13 @@ for div in divs:
         break
     
 print locationDictionary
+print "\n\n\n"
+print categoryDictionary
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
