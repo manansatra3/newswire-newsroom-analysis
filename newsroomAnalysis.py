865673d38b449803cf18e94db7bf18e9faca6be0
Manan Satra <msatra@stevens.edu>
2018-08-25 20:19:02 -0400

    tags dictionary

--- newsroomAnalysis.py
+++ newsroomAnalysis.py
@@ -1,20 +1,20 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[58]:
 
 import requests
 from bs4 import BeautifulSoup
 
 
-# In[2]:
+# In[59]:
 
 locationDictionary={}
 categoryDictionary={}
 tagsDictionary={}
 
 
-# In[3]:
+# In[60]:
 
 dateInput=raw_input("Enter a VALID date in MM-DD-YYYY format. Example: 01-31-2018\n")
 print(dateInput)
@@ -26,14 +26,14 @@ year=int(dateInput.split("-")[2])
 # print(year)
 
 
-# In[4]:
+# In[61]:
 
 page = requests.get("https://www.newswire.com/newsroom")    
 if page.status_code==200:   
     soup = BeautifulSoup(page.content, 'html.parser') 
 
 
-# In[7]:
+# In[70]:
 
 def monthToIntMonth(articleMonth):
     switcher = {
@@ -67,7 +67,6 @@ def getArticleData(soupArticle):
     for loc in locList:
         loc=loc.encode('ascii').lstrip().rstrip()
         location.append(loc)
-    print location
     for loc in location:     
         if loc in locationDictionary.keys():
             locationDictionary[loc]+=1
@@ -75,49 +74,66 @@ def getArticleData(soupArticle):
             locationDictionary[loc]=1
         
     catSoup=soupArticle.select("p[class=mb-0]")[0]
-    catList=str(catSoup.select("a")[0].get_text().encode('ascii').lower()).split(",")
-    for cat in catList:
-        cat=cat.lstrip()
-        if cat in categoryDictionary.keys():
-            categoryDictionary[cat]+=1
-        else:
-            categoryDictionary[cat]=1
+    catListLen=len(catSoup.select("a"))
+    for i in range(0,catListLen):
+        catList=str(catSoup.select("a")[i].get_text().encode('ascii').lower()).split(",")
+        for cat in catList:
+            cat=cat.lstrip()
+            if cat in categoryDictionary.keys():
+                categoryDictionary[cat]+=1
+            else:
+                categoryDictionary[cat]=1
+
+    
+    tagSoup=soupArticle.select("p[class=mb-0]")[1]
+    tagListLen=len(tagSoup.select("a"))
+    for i in range(0,tagListLen):
+        tagList=str(tagSoup.select("a")[i].get_text().encode('ascii').lower()).split(",")
+        for tag in tagList:
+            if tag in tagsDictionary.keys():
+                tagsDictionary[tag]+=1
+            else:
+                tagsDictionary[tag]=1
 
 
-# In[8]:
+# In[71]:
 
-divs=soup.select("div.news-item-body")
-
-for div in divs:
-    p=div.select("time.ln-date")[0]
-    articleDate=p.get_text().split(" ")
-    articleDay=int(str(articleDate[1]).replace(',',''))
-#     print "Day:", articleDay
-    articleMonth=int(monthToIntMonth(str(articleDate[0]).strip()))
-#     print "Month:", articleMonth
-    articleYear=int(articleDate[2])
-#     print "Year:", articleYear
-    print str(articleMonth) + "/" + str(articleDay) + "/" + str(articleYear)
-    if(articleYear>=year):
-        if(articleMonth>=month):
-            if(articleDay>=date):
-                url="https://www.newswire.com/"+div.select("a.content-link")[0]['href']
-                print url
-                soupArticle=getSoupArticle(url)
-                getArticleData(soupArticle)
+nextPageTraverse=True
+while(nextPageTraverse):
+    divs=soup.select("div.news-item-body")
+    for div in divs:
+        p=div.select("time.ln-date")[0]
+        articleDate=p.get_text().split(" ")
+        articleDay=int(str(articleDate[1]).replace(',',''))
+    #     print "Day:", articleDay
+        articleMonth=int(monthToIntMonth(str(articleDate[0]).strip()))
+    #     print "Month:", articleMonth
+        articleYear=int(articleDate[2])
+    #     print "Year:", articleYear
+        print str(articleMonth) + "/" + str(articleDay) + "/" + str(articleYear)
+        if(articleYear>=year):
+            if(articleMonth>=month):
+                if(articleDay>=date):
+                    url="https://www.newswire.com/"+div.select("a.content-link")[0]['href']
+                    print url
+                    soupArticle=getSoupArticle(url)
+                    getArticleData(soupArticle)
+                else:
+                    break
             else:
                 break
         else:
             break
-    else:
-        break
-    
-print locationDictionary
-print "\n\n\n"
-print categoryDictionary
+        
+
+    print locationDictionary
+    print "\n\n\n"
+    print categoryDictionary
+    print "\n\n\n"
+    print tagsDictionary
 
 
-# In[ ]:
+# In[73]:
 
 
 
