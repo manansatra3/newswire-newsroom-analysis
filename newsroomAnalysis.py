94b9b504952ce760b95649ba40c7f3287a715630
Manan Satra <msatra@stevens.edu>
2018-08-26 20:10:55 -0400

    date calculations

--- newsroomAnalysis.py
+++ newsroomAnalysis.py
@@ -1,44 +1,67 @@
 
 # coding: utf-8
 
-# In[79]:
+# In[18]:
+
+##Imports
 
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
-import numpy as np
-import csv
 import os
+from datetime import date as dateClass, timedelta
 
 
-# In[3]:
+# In[19]:
+
+## Declaring dictionaries for storage of data
 
 locationDictionary={}
 categoryDictionary={}
 tagsDictionary={}
 
 
-# In[4]:
+# In[20]:
 
-dateInput=raw_input("Enter a VALID date in MM-DD-YYYY format. Example: 01-31-2018\n")
-print(dateInput)
-month=int(dateInput.split("-")[0])
-date=int(dateInput.split("-")[1])
-year=int(dateInput.split("-")[2])
-# print(date)
-# print(month)
-# print(year)
+#Date Calculations
+
+dateCorrect=False
+while dateCorrect!=True:
+    dateInput=raw_input("Enter a VALID date in MM-DD-YYYY format. Example: 01-31-2018 or LEAVE BLANK for DEFAULTof past 7 days\n")
+    if(dateInput==''):
+        print "No Date Provided. Using a default of past 7 days data."
+        dateInput1=dateClass.today() - timedelta(days=7)
+        year= dateInput1.year
+        month= dateInput1.month
+        date= dateInput1.day
+        print "Starting to scrape newsroom data for analysis...."
+        break
+    else:
+        month=int(dateInput.split("-")[0])
+        date=int(dateInput.split("-")[1])
+        year=int(dateInput.split("-")[2])
+        dateInput1=dateClass(year,month,date)
+        if (dateInput1 <= dateClass.today()):
+            dateCorrect=True
+            print "Starting to scrape newsroom data for analysis...."
+        else:
+            print "Date entered is after today's date. Enter today's date or past date"
 
 
-# In[5]:
+# In[21]:
+
+#Making a request to the webpage and getting beautiful soup object
 
 page = requests.get("https://www.newswire.com/newsroom")    
 if page.status_code==200:   
-    soup = BeautifulSoup(page.content, 'html.parser') 
+    soup = BeautifulSoup(page.content, 'html.parser')
+else:
+    print "HTTP Request Rejected by: https://www.newswire.com/newsroom\tTry again later"
 
 
-# In[6]:
+# In[22]:
 
+#function to map months to month number
 def monthToIntMonth(articleMonth):
     switcher = {
             "Jan":1,
@@ -56,16 +79,18 @@ def monthToIntMonth(articleMonth):
         }
     return switcher.get(articleMonth, "Invalid month")
 
+#function to generate soup object for inidivual article page
 def getSoupArticle(url):
     pageArticle = requests.get(url)    
     if pageArticle.status_code==200:   
         soupArticle = BeautifulSoup(pageArticle.content, 'html.parser')
     else:
-        print "Error while parsing the Article Page with URL: "+url  
+        print "HTTP Request Rejected by: "+url+"\tTry again later"
     return soupArticle
 
+#function to extract location, category and tag data from article URLs
 def getArticleData(soupArticle):
-    
+    #getting location data
     locList=soupArticle.select("p strong.date-line.color-pr")[0].get_text().strip().lower().split(",")[:-2]
     location=[]
     for loc in locList:
@@ -76,7 +101,8 @@ def getArticleData(soupArticle):
             locationDictionary[loc]+=1
         else:
             locationDictionary[loc]=1
-        
+    
+    #getting category data
     catSoup=soupArticle.select("p[class=mb-0]")[0]
     catListLen=len(catSoup.select("a"))
     for i in range(0,catListLen):
@@ -88,7 +114,7 @@ def getArticleData(soupArticle):
             else:
                 categoryDictionary[cat]=1
 
-    
+    #getting tag data
     tagSoup=soupArticle.select("p[class=mb-0]")[1]
     tagListLen=len(tagSoup.select("a"))
     for i in range(0,tagListLen):
@@ -100,8 +126,9 @@ def getArticleData(soupArticle):
                 tagsDictionary[tag]=1
 
 
-# In[7]:
+# In[23]:
 
+#traversing articles using beautiful soup until the appropriate date
 nextPageTraverse=True
 while(nextPageTraverse):
     divs=soup.select("div.news-item-body")
@@ -109,17 +136,13 @@ while(nextPageTraverse):
         p=div.select("time.ln-date")[0]
         articleDate=p.get_text().split(" ")
         articleDay=int(str(articleDate[1]).replace(',',''))
-    #     print "Day:", articleDay
         articleMonth=int(monthToIntMonth(str(articleDate[0]).strip()))
-    #     print "Month:", articleMonth
         articleYear=int(articleDate[2])
-    #     print "Year:", articleYear
-        print str(articleMonth) + "/" + str(articleDay) + "/" + str(articleYear)
         if(articleYear>=year):
             if(articleMonth>=month):
                 if(articleDay>=date):
                     url="https://www.newswire.com/"+div.select("a.content-link")[0]['href']
-                    print url
+                    print str(articleMonth) + "/" + str(articleDay) + "/" + str(articleYear)+" -- "+url
                     soupArticle=getSoupArticle(url)
                     getArticleData(soupArticle)
                 else:
@@ -139,24 +162,24 @@ while(nextPageTraverse):
         if page.status_code==200:   
             soup = BeautifulSoup(page.content, 'html.parser') 
     except:
-        print 'No Next Page'
+        print 'No Next Page. End of results'
         nextPageTraverse=False
-    
 
 
-# In[71]:
+# In[24]:
 
-print "Printing Location Analysis Graph and Table. The plot has also been saved as a LocationAnalysis.png file locally in the CWD: "+os.getcwd()
+print "Location Analysis Graph is save locally in your CWD: "+os.getcwd()+"\LocationAnalysis.png"
+print "Printing the Location Analysis Table:\n"
+
+#generating LocationAnalysis bar graph
 fig, ax = plt.subplots(figsize=(35,12))
-plt.bar(np.arange(len(locationDictionary)), locationDictionary.values(), align='center')
-plt.xticks(np.arange(len(locationDictionary)), locationDictionary.keys(),rotation='vertical', fontsize='12')
+plt.bar(range(len(locationDictionary)), locationDictionary.values(), align='center')
+plt.xticks(range(len(locationDictionary)), locationDictionary.keys(),rotation='vertical', fontsize='12')
 plt.xlabel('Locations')
 plt.ylabel('Number of Articles')
 plt.savefig('LocationAnalysis.png')
-plt.ion()
-plt.show()
 
-
+#generating location analysis frequency table
 locDict1={}
 maxLen=0
 for k,v in locationDictionary.iteritems():
@@ -171,18 +194,17 @@ for k,v in locDict1.iteritems():
     print k+' '*(maxLen-len(k)+10)+"|"+' '*10+str(v)
 
 
-# In[72]:
+# In[25]:
 
-print "Printing Category Analysis Graph and Table. The plot has also been saved as a CategoryAnalysis.png file locally in the CWD: "+os.getcwd()
+print "Category Analysis Graph is save locally in your CWD: "+os.getcwd()+"\CategoryAnalysis.png"
+print "Printing the Category Analysis Table:\n"
 
 fig, ax = plt.subplots(figsize=(35,12))
-plt.bar(np.arange(len(categoryDictionary)), categoryDictionary.values(), align='center')
-plt.xticks(np.arange(len(categoryDictionary)), categoryDictionary.keys(),rotation='vertical', fontsize='12')
+plt.bar(range(len(categoryDictionary)), categoryDictionary.values(), align='center')
+plt.xticks(range(len(categoryDictionary)), categoryDictionary.keys(),rotation='vertical', fontsize='12')
 plt.xlabel('Categories')
 plt.ylabel('Number of Articles')
 plt.savefig('CategoryAnalysis.png')
-# plt.ion()
-plt.show()
 
 maxLen=0
 for k,v in categoryDictionary.iteritems():
@@ -197,9 +219,9 @@ for k,v in categoryDictionary.iteritems():
     print k+' '*(maxLen-len(k)+10)+"|"+' '*10+str(v)
 
 
-# In[78]:
+# In[26]:
 
-print "The Tags are mostly unique for each article and thus plotting a bar graph wouldn't be feasible/readble.We thus only have a table for the same\n\n"
+print "The Tags are mostly unique for each article and thus plotting a bar graph wouldn't be feasible/readble. We thus only have a table for the same\n\n"
 
 maxLen=0
 for k,v in tagsDictionary.iteritems():
@@ -210,14 +232,7 @@ print "Tags"+' '*(maxLen+6)+"| Number of Article"
 print '-'*(maxLen+32)
 for k,v in tagsDictionary.iteritems():
     print k+' '*(maxLen-len(k)+10)+"|"+' '*10+str(v)
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
+    
+    
+print "Please DONT FORGET to take a look at the LocationAnalysis and CategoryAnalysis plots saved locally in your CWD: "+os.getcwd()
 
